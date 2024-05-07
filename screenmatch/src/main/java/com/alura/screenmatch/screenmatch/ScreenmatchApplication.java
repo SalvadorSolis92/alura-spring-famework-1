2e9a6810a3d540a28af7dfce913919bf9baa7028
SalvadorSolis92 <angelcantu086@gmail.com>
2024-05-06 22:18:09 -0600

    Consumo de datos por temporadas

--- screenmatch/src/main/java/com/alura/screenmatch/screenmatch/ScreenmatchApplication.java
+++ screenmatch/src/main/java/com/alura/screenmatch/screenmatch/ScreenmatchApplication.java
@@ -2,11 +2,15 @@ package com.alura.screenmatch.screenmatch;
 
 import com.alura.screenmatch.screenmatch.model.DatosEpisodio;
 import com.alura.screenmatch.screenmatch.model.DatosSeries;
+import com.alura.screenmatch.screenmatch.model.DatosTemporadas;
 import com.alura.screenmatch.screenmatch.service.ConsumoAPI;
 import com.alura.screenmatch.screenmatch.service.ConvierteDatos;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication implements CommandLineRunner {
@@ -18,7 +22,7 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		var consumoAPI = new ConsumoAPI();
-		var json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?i=tt3896198&apikey=c131c757");
+		var json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&apikey=c131c757");
 
 		System.err.println(json);
 
@@ -31,7 +35,15 @@ public class ScreenmatchApplication implements CommandLineRunner {
 
 		json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&Season=1&episode=1&apikey=c131c757");
 		DatosEpisodio datosEpisodio = conversor.obtenerDatos(json, DatosEpisodio.class);
-
 		System.out.println(datosEpisodio);
+
+		List<DatosTemporadas> temporadas = new ArrayList<>();
+		for (int i = 1; i <= datos.totalDeTemporadas(); i++) {
+			json = consumoAPI.obtenerDatos("http://www.omdbapi.com/?t=game+of+thrones&Season="+i+"&apikey=c131c757");
+			var datosTemporada = conversor.obtenerDatos(json, DatosTemporadas.class);
+			temporadas.add(datosTemporada);
+		}
+
+		temporadas.forEach(System.out::println);
 	}
 }
